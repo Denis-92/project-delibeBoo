2a6968221473dc02bb44a8c0c03f9c0a549d5763
Denis-92 <d.mozenic@gmail.com>
2022-12-06 15:30:56 +0100

    add fillable in Plate.php

--- app/Plate.php
+++ app/Plate.php
@@ -7,5 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Plate extends Model
 {
     //
-    // TO DO fillable
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'visibility',
+        'image',
+        'slug',
+        'restaurant_id'
+    ]
+
+    // TODO
+    // public function restaurant() {
+    //     return $this->belongsTo('App\Restaurant');
+    // }
 }
