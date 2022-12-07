ff3827384c31bc64ae9a0b87c461f80f2451d74d
MartinaBasile <martinaleandribasile@gmail.com>
2022-12-07 12:16:02 +0100

    resturant

--- resources/views/admin/resturants/index.blade.php
+++ resources/views/admin/resturants/index.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    @foreach ($resturants as $resturant)
-        <p>{{ $resturant->name }}</p>
+    @foreach ($resturants as $rest)
+        @if ($rest->user_id === $user->id)
+            <p>{{ $rest->name }}</p>
+            <a href="{{ route('admin.resturants.show', $rest->id) }}">Dettagli</a>
+        @endif
     @endforeach
+
+    <div>
+        <a href="{{ route('admin.resturants.create') }}">Crea Nuovo </a>
+    </div>
 @endsection
