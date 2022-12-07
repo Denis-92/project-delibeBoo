055db26a015c5acfd76d56c5f646b4d362bd711e
Angelo <Angelodimauro91@gmail.com>
2022-12-07 12:13:11 +0100

    barra "animate" (add card)

--- public/js/app.js
+++ public/js/app.js
@@ -1977,7 +1977,43 @@ var staticRenderFns = [function () {
     _c = _vm._self._c;
   return _c("section", [_c("div", {
     staticClass: "container-animate"
-  })]);
+  }, [_c("div", {
+    staticClass: "card-preview"
+  }), _vm._v(" "), _c("div", {
+    staticClass: "card-preview"
+  }), _vm._v(" "), _c("div", {
+    staticClass: "card-preview"
+  }), _vm._v(" "), _c("div", {
+    staticClass: "card-preview"
+  }), _vm._v(" "), _c("div", {
+    staticClass: "card-preview"
+  }), _vm._v(" "), _c("div", {
+    staticClass: "card-preview"
+  }), _vm._v(" "), _c("div", {
+    staticClass: "card-preview"
+  }), _vm._v(" "), _c("div", {
+    staticClass: "card-preview"
+  }), _vm._v(" "), _c("div", {
+    staticClass: "card-preview"
+  }), _vm._v(" "), _c("div", {
+    staticClass: "card-preview"
+  }), _vm._v(" "), _c("div", {
+    staticClass: "card-preview"
+  }), _vm._v(" "), _c("div", {
+    staticClass: "card-preview"
+  }), _vm._v(" "), _c("div", {
+    staticClass: "card-preview"
+  }), _vm._v(" "), _c("div", {
+    staticClass: "card-preview"
+  }), _vm._v(" "), _c("div", {
+    staticClass: "card-preview"
+  }), _vm._v(" "), _c("div", {
+    staticClass: "card-preview"
+  }), _vm._v(" "), _c("div", {
+    staticClass: "card-preview"
+  }), _vm._v(" "), _c("div", {
+    staticClass: "card-preview"
+  })])]);
 }];
 render._withStripped = true;
 
@@ -6480,7 +6516,7 @@ exports = module.exports = __webpack_require__(/*! ../../../node_modules/css-loa
 
 
 // module
-exports.push([module.i, ".container-animate[data-v-a8b8b442] {\n  width: 100%;\n  height: 25vh;\n  background-color: rgba(255, 0, 0, 0.521);\n  position: relative;\n  bottom: 50px;\n  z-index: 800;\n}", ""]);
+exports.push([module.i, ".container-animate[data-v-a8b8b442] {\n  width: 100%;\n  height: 25vh;\n  background-color: rgba(255, 0, 0, 0.521);\n  position: relative;\n  bottom: 50px;\n  z-index: 800;\n  display: flex;\n  justify-content: space-around;\n  align-items: center;\n  flex-wrap: wrap;\n  gap: 10px;\n}\n.container-animate .card-preview[data-v-a8b8b442] {\n  width: 10%;\n  height: 45%;\n  background-color: rgb(124, 0, 196);\n}", ""]);
 
 // exports
 
--- resources/js/components/AnimationGuest.vue
+++ resources/js/components/AnimationGuest.vue
@@ -1,6 +1,25 @@
 <template>
     <section>
         <div class="container-animate">
+            <div class="card-preview"></div>
+            <div class="card-preview"></div>
+            <div class="card-preview"></div>
+            <div class="card-preview"></div>
+            <div class="card-preview"></div>
+            <div class="card-preview"></div>
+            <div class="card-preview"></div>
+            <div class="card-preview"></div>
+            <div class="card-preview"></div>
+            <div class="card-preview"></div>
+            <div class="card-preview"></div>
+            <div class="card-preview"></div>
+            <div class="card-preview"></div>
+            <div class="card-preview"></div>
+            <div class="card-preview"></div>
+            <div class="card-preview"></div>
+            <div class="card-preview"></div>
+            <div class="card-preview"></div>
+
 
         </div>
     </section>
@@ -15,6 +34,16 @@
             position: relative;
             bottom: 50px;
             z-index: 800;
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            flex-wrap: wrap;
+            gap: 10px;
+            .card-preview{
+                width: 10%;
+                 height: 45%;
+                background-color: rgb(124, 0, 196);
+            }
         }
     </style>
     
