c9f98c63e5eb4407ef24bd9649d5a0a31e6a9c7d
Angelo <Angelodimauro91@gmail.com>
2022-12-13 17:01:20 +0100

    home (input border fix)

--- resources/js/components/HeaderGuest.vue
+++ resources/js/components/HeaderGuest.vue
@@ -264,6 +264,7 @@ header{
                         width: 100%;
                         height: 100%;
                         border: none;
+                        outline: none;
                     }
                 }
                 .BDX{
