e51227a45ea83a3119dd7f4e54bbddd50f399e58
Denis-92 <d.mozenic@gmail.com>
2022-12-15 11:09:42 +0100

    add style img and infoResturants component

---
+++ resources/js/components/resturantsComponents/infoResturants.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div v-for="ristorante, index in ristoranti" :key="index">
+
+            <div>
+                <img class="restaurant-image" :src="`storage/${ristorante.image}`" alt="">
+            </div>
+
+            <div>
+                <div>
+                    <h5>{{ ristorante.name }}</h5>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "infoResturants",
+    data() {
+        return {
+            url: 'api/resturants',
+            ristoranti: undefined,
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- resources/js/pages/ResturantMenu.vue
+++ resources/js/pages/ResturantMenu.vue
@@ -1,25 +1,30 @@
 <template>
   <div>
-      <headerResturants />
-     <div class="d-flex">
+    <headerResturants />
+    <infoResturants />
+    <div class="d-flex">
       <div v-if="resturant != undefined">
-        <div v-for="plate, index in resturant.plates" :key="index" >
-                <div v-if="!plate.hidden">
+        <div v-for="plate, index in resturant.plates" :key="index">
+          <div v-if="!plate.hidden">
 
-                    <img  :src="`storage/${plate.image}`" alt="">
-                    <h5>{{plate.name}}</h5>
-                    <p>Descrizione:</p>
-                    <p>{{plate.description}} </p>
-                    <p>Prezzo:</p>
-                    <p>{{plate.price}}</p>
-                </div>
+            <div class="card-img">
+              <img :src="`storage/${plate.image}`" alt="">
+            </div>
+            <h5>{{ plate.name }}</h5>
+            <p>Descrizione:</p>
+            <p>{{ plate.description }} </p>
+            <p>Prezzo:</p>
+            <p>{{ plate.price }}</p>
+            <button @click="order(plate)" type="button" class="btn btn-outline-success mt-1 mb-5">Aggiungi al
+              carrello</button>
+          </div>
 
-                <button @click="order(plate)" type="button" class="btn btn-outline-success mt-1 mb-5">Aggiungi al carrello</button>
+
         </div>
       </div>
-      <OrderComponent :plateToadd="plateOrder"/>
-     </div>
-      <FooterResturants />
+      <OrderComponent :plateToadd="plateOrder" />
+    </div>
+    <FooterResturants />
   </div>
 </template>
 
@@ -27,43 +32,53 @@
 import headerResturants from '../components/resturantsComponents/headerResturants.vue';
 import FooterResturants from '../components/resturantsComponents/FooterResturants.vue';
 import OrderComponent from '../components/resturantsComponents/OrderComponent.vue';
+import infoResturants from '../components/resturantsComponents/infoResturants.vue';
+
 export default {
-name:'ResturantMenu',
-components: { headerResturants, FooterResturants, OrderComponent },
-data(){
-  return{
-    resturant:undefined,
-    plateOrder:[]
+  name: 'ResturantMenu',
+  components: { headerResturants, FooterResturants, OrderComponent, infoResturants },
+  data() {
+    return {
+      resturant: undefined,
+      plateOrder: []
+    }
+  },
+  methods: {
+    getResturant(url) {
+      axios.get(url).then(({ data }) => {
+
+        console.log(data)
+        this.resturant = data
+
+      })
+    },
+    order(plate) {
+      let counter = 1
+      let data =
+      {
+        'plate': plate,
+        'counter': counter
+      }
+      this.plateOrder.push(data)
+      console.log(this.plateOrder)
+      console.log(this.plateOrder[0].counter)
+    },
+  },
+
+  mounted() {
+    const slug = this.$route.params.slug;
+    this.getResturant('api/resturants/' + slug);
   }
-},
-methods:{
-  getResturant(url){
-        axios.get(url).then(({data})=>{
-
-            console.log(data)
-            this.resturant = data
-
-        })},
-        order(plate){
-          let counter=1
-          let data=
-            {
-              'plate': plate,
-              'counter': counter
-            }
-          this.plateOrder.push(data)
-          console.log(this.plateOrder)
-          console.log(this.plateOrder[0].counter)
-        },
-},
-
-mounted(){
-  const slug = this.$route.params.slug;
-  this.getResturant('api/resturants/' +slug);
-}
 }
 </script>
 
 <style>
+.card-img {
+  width: 300px;
+}
 
+img {
+  width: 100%;
+  height: auto;
+}
 </style>
