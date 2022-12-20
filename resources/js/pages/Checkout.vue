71ef0f7120330e0d7a49b8eb4e0ae59a6f9f1373
valentinaiori <valentinaiori@live.it>
2022-12-20 16:13:41 +0100

    Add route redirect resturant

--- resources/js/pages/Checkout.vue
+++ resources/js/pages/Checkout.vue
@@ -1,11 +1,24 @@
 <template>
     <div>
-        <h2 class=" text-center mt-5">Pagamento avvenuto con successo</h2>
+        <h2 class="ml-5 mt-5">Pagamento avvenuto con successo</h2>
+        <button type="button" @click='returnResturant()' class="btn btn-outline-info ml-5">Torna alla lista ristoranti</button>
     </div>
 </template>
 
 <script>
 export default{
-    name: 'Checkout'
+    name: 'Checkout',
+
+    methods: {
+        returnResturant(){
+            window.location.href='/#/resturants'
+        }
+    }
 }
+
 </script>
+
+<style lang="scss" scoped>
+
+
+</style>
